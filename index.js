4c85f257793780913f7b5ba0c0a854e6330f06cc
scoldridge <25624891+scoldridge@users.noreply.github.com>
2022-05-26 11:30:43 +1000

    more jump, remove DS_store

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let sendFeeAmount = 0;
 let receiveAmount = 0;
 
 const BATCH_TX_COUNT = 5000; // Process this many transactions at once
-const JUMP_TX_COUNT = 100000; // Move this many transactions forward
+const JUMP_TX_COUNT = 200000; // Move this many transactions forward
 const balances = [];
 
 let blocks = 0; // This is required for coins that don't store blockheight
